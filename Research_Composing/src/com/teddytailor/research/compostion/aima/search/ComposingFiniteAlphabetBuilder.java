d2a4fb48ba91e2c084159210e8a4a24c9c06604d
wanglxi1 <wanglxi1@7ace194e-5dc1-4163-9bda-fbc42745c31e>
2013-08-16 04:06:15 +0000

    修改 order 随机范围

--- Research_Composing/src/com/teddytailor/research/compostion/aima/search/ComposingFiniteAlphabetBuilder.java
+++ Research_Composing/src/com/teddytailor/research/compostion/aima/search/ComposingFiniteAlphabetBuilder.java
@@ -7,15 +7,17 @@ import com.teddytailor.research.compostion.aima.data.ComposingBoard;
 public class ComposingFiniteAlphabetBuilder implements FiniteAlphabetBuilder<Integer> {
 
 	protected Random random;
+	private int len;
 	
 	public ComposingFiniteAlphabetBuilder(ComposingBoard board) {
 		this.random = new Random();
+		this.len = board.getModels().size();
 	}
 	
 	@Override
 	public Integer build(Integer origin) {
 		boolean reversal = random.nextBoolean();
-		int order = random.nextInt(100);
+		int order = random.nextInt(this.len);
 		
 		return order * (reversal? -1: 1);
 	}
